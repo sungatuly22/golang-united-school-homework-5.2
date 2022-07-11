ed666dcc900ae8cb8b86f83933559982708760b2
sungatuly22 <sungatsson22@gmail.com>
2022-07-11 17:55:13 +0600

    fifth task

--- cache.go
+++ cache.go
@@ -1,23 +1,54 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	storage map[string]ValueStorage
+}
+
+type ValueStorage struct {
+	value    string
+	deadline *time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		storage: make(map[string]ValueStorage),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	value, ok := c.storage[key]
+	if !ok {
+		return "", false
+	}
+	if value.deadline != nil && !value.deadline.After(time.Now()) {
+		delete(c.storage, key)
+		return "", false
+	}
+	return value.value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.storage[key] = ValueStorage{value: value, deadline: nil}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var result []string
+	currentTime := time.Now()
+
+	for key, value := range c.storage {
+		if value.deadline != nil && !value.deadline.After(currentTime) {
+			delete(c.storage, key)
+			continue
+		}
+		result = append(result, key)
+	}
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.storage[key] = ValueStorage{value: value, deadline: &deadline}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module homework5/2
+
+go 1.16
